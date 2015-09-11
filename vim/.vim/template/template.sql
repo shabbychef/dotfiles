025413afa0e2c5d3e80ab639b274409d45cd15b8
Steven Pav <steven@gilgamath.com>
2015-09-11 15:29:56 -0700

    sql template

---
+++ vim/.vim/template/template.sql
@@ -0,0 +1,29 @@
+:% s/MY_FULL_NAME/\=g:my_name/ge
+:% s/MY_EMAIL/\=g:my_email/ge
+:% s/DATE/\=strftime('%Y.%m.%d')/ge
+:% s/YEAR/\=strftime('%Y')/ge
+:% s/EMAILATAT/\=substitute($EMAIL,"@","@@","")/ge
+:% s/EMAIL/\=$EMAIL/ge
+:% s/THEFILENAME/\=expand("%:t")/g
+:% s/THEFUNCNAME/\=expand("%:t:r")/g
+:% s/CLASS/\=expand("%:p:h:t:r")/g
+:if search('<+CURSOR+>')
+:  normal! "_da>
+:endif
+:finish
+#
+# Created: DATE
+# Copyright: MY_FULL_NAME, YEAR
+# Author: MY_FULL_NAME <EMAIL>
+# Comments: MY_FULL_NAME
+
+CREATE TABLE `Fun` (
+	`id` int(11) unsigned NOT NULL auto_increment,
+	`name` varchar(100) NOT NULL,
+	PRIMARY KEY (`id`),
+	UNIQUE KEY `uni_name` (`name`)
+) ENGINE=InnoDB;
+CREATE INDEX fun_index ON Fun(name)
+
+#for vim modeline: (do not edit)
+# vim:ts=2:sw=2:tw=79:fdm=indent:cms=#%s:syn=mysql:ft=mysql:ai:si:cin:nu:fo=croql:cino=p0t0c5(0:
