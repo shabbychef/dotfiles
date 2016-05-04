b98b2efe25e371a2059dd2a71c57918462d076c9
Steven Pav <shabbychef@gmail.com>
2016-05-03 22:30:54 -0700

    header file template

---
+++ vim/.vim/template/template.h
@@ -0,0 +1,35 @@
+:silent! % s/MY_FULL_NAME/\=g:my_name/ge
+:silent! % s/MY_EMAIL/\=g:my_email/ge
+:silent! % s/DATE/\=strftime('%Y.%m.%d')/ge
+:silent! % s/YEAR/\=strftime('%Y')/ge
+:silent! % s/EMAILATAT/\=substitute($EMAIL,"@","@@","")/ge
+:silent! % s/EMAIL/\=$EMAIL/ge
+:silent! % s/THEFILENAME/\=expand("%:t")/g
+:silent! % s/THEFUNCNAME/\=expand("%:t:r")/g
+:silent! % s/THECAPFUNCNAME/\=toupper(expand("%:t:r"))/g
+:silent! % s/CLASS/\=expand("%:p:h:t:r")/g
+:if search('<+CURSOR+>')
+:  normal! "_da>
+:endif
+:finish
+
+/*
+
+  Created: DATE
+  Copyright: MY_FULL_NAME, YEAR
+  Author: MY_FULL_NAME <EMAIL>
+  Comments: MY_FULL_NAME
+*/
+
+#ifndef __DEF_THECAPFUNCNAME__
+#define __DEF_THECAPFUNCNAME__
+
+#include <math.h>
+#include <string.h>
+
+#endif /* __DEF_THECAPFUNCNAME__ */
+
+<+CURSOR+>
+
+//for vim modeline: (do not edit)
+// vim:ts=2:sw=2:tw=79:fdm=marker:fmr=FOLDUP,UNFOLD:cms=//%s:tags=.c_tags;:syn=cpp:ft=cpp:mps+=<\:>:ai:si:cin:nu:fo=croql:cino=p0t0c5(0:
